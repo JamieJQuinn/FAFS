61dfdeb15e7ff5654b295ad999bff175b935a461
Jamie Quinn <jamiejquinn@jamiejquinn.com>
2021-10-13 14:13:42 +0100

    Add ability to render streamflows on top of imshow plots

--- visualisation/renderHDF5.py
+++ visualisation/renderHDF5.py
@@ -1,4 +1,5 @@
 import h5py
+import numpy as np
 import matplotlib.pyplot as plt
 import argparse
 
@@ -6,17 +7,28 @@ def main():
     parser = argparse.ArgumentParser(description='Render hdf5 file')
     parser.add_argument('filename',
                         help='filename')
-    parser.add_argument('--variable',
-                        help='variable to plot')
+    parser.add_argument('--imshow', default='',
+                        help='variable to plot via imshow')
+    parser.add_argument('--streamplot', nargs=2, default=[],
+                        help='components of vector to plot via streamlines')
 
     args = parser.parse_args()
     fname = args.filename
 
     with h5py.File(fname, "r") as hf:
-        data = hf[args.variable][:, :].T
-        im = plt.imshow(data, origin='lower')
-        plt.colorbar(im)
-        plt.show()
+        if args.imshow:
+            data = hf[args.imshow][1:-1, 1:-1].T
+            im = plt.imshow(data, origin='lower', extent=(0, 1, 0, 1))
+            plt.colorbar(im)
+        if args.streamplot:
+            v1 = hf[args.streamplot[0]][1:-1, 1:-1].T
+            v2 = hf[args.streamplot[1]][1:-1, 1:-1].T
+            x = np.linspace(0, 1, v1.shape[0])
+            y = np.linspace(0, 1, v1.shape[1])
+            X, Y = np.meshgrid(x, y)
+            plt.streamplot(X, Y, v1, v2)
+
+    plt.show()
 
 
 main()
