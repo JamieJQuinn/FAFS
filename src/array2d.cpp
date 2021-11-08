cf2da5841f0d8cd99787862a8a2ad5223fd4c16f
Jamie Quinn <jamiejquinn@jamiejquinn.com>
2021-11-08 21:33:12 +0000

    Fix incorrect type in array sum

--- src/array2d.cpp
+++ src/array2d.cpp
@@ -52,13 +52,14 @@ void Array::applyKernel(kernelFn fn, Array& out) const {
 }
 
 real Array::sum() const {
-  int result = 0;
+  real result = 0;
 #pragma omp parallel for collapse(2) schedule(static) reduction(+:result)
   for (int i=0; i<nx; ++i) {
     for (int j=0; j<ny; ++j) {
       result += (*this)(i,j);
     }
   }
+  return result;
 }
 
 void Array::applyKernel(kernelFnInPlaceInput fn, const Array& in) {
